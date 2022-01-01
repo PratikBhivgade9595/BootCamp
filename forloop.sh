fb8dbeca59d276b88fc69432a5669aadcc216949
PratikBhivgade9595 <pratikg9595@gmail.com>
2022-01-01 20:21:22 +0530

    add forloop.sh

--- forloop.sh
+++ forloop.sh
@@ -2,18 +2,6 @@
 
 for file in `ls *.txt`;
 do
-<<<<<<< HEAD
-    Foldername=`echo $file | awk -F. '{print $1}'`;
-    #echo "checking for already existing directory started";
-    if [ -d $foldername ];
-    then
-       rm -R $foldername;
-    fi
-    #echo creating folder ${foldername};
-    mkdir $foldername;
-    #echo copying ${file} to ${foldername};
-    cp $file $foldername;
-=======
     foldername=`echo $file | awk -F. '{print $1}'`;
     #echo "checking for already existing directory started";
     if [ -d $foldername ];
@@ -24,5 +12,4 @@ do
     mkdir $foldername;
     #echo copying ${file} to ${foldername};
     cp $file $foldername;
->>>>>>> 578abdaca3208565e965f9fc9c4e2d79d498b5ee
 done
