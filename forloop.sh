8b45ffda6cd382944f056e301c3e9af21302b128
PratikBhivgade9595 <96899328+PratikBhivgade9595@users.noreply.github.com>
2022-01-01 18:37:00 +0530

    Update forloop.sh
    I have change foldername

--- forloop.sh
+++ forloop.sh
@@ -2,14 +2,14 @@
 
 for file in `ls *.txt`;
 do
-    folderName=`echo $file | awk -F. '{print $1}'`;
+    foldername=`echo $file | awk -F. '{print $1}'`;
     #echo "checking for already existing directory started";
-    if [ -d $folderName ];
+    if [ -d $foldername ];
     then
-       rm -R $folderName;
+       rm -R $foldername;
     fi
-    #echo creating folder ${folderName};
-    mkdir $folderName;
-    #echo copying ${file} to ${folderName};
-    cp $file $folderName;
+    #echo creating folder ${foldername};
+    mkdir $foldername;
+    #echo copying ${file} to ${foldername};
+    cp $file $foldername;
 done
